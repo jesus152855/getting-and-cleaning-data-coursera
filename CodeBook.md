37444be279295a34939be62458cb5f67146a46ed
jesus152855 <jdrodriguez152855@gmail.com>
2017-11-16 13:54:06 -0500

    Solution final assignment

---
+++ CodeBook.md
@@ -0,0 +1,104 @@
+## Code Book
+
+This code book summarizes the resulting data fields in `tidy.txt`.
+
+## Steps
+
+1. Download the data set, if the file doesn't exist
+2. Unzip the data set into my chosen working directory
+3. Load activity labels and features
+4. Extract only the features on mean and standard deviation
+5. Load test and training data sets into data tables
+6. Merge datasets and add correct labels
+7. Turn activities and subjects into factors
+8. Calculating the average of the measurements
+9. Export the tidyData set 
+
+Please refer to [run_analysis.R] (https://github.com/rwstang/Getting-and-Cleaning-Data-Assignment/blob/master/run_analysis.R) for implementation details.
+
+## Identifiers
+
+* `subject` - The ID of the test subject
+* `activity` - The type of activity performed when the corresponding measurements were taken
+
+## Measurements
+
+* `tBodyAccMeanX`
+* `tBodyAccMeanY`
+* `tBodyAccMeanZ`
+* `tBodyAccStdX`
+* `tBodyAccStdY`
+* `tBodyAccStdZ`
+* `tGravityAccMeanX`
+* `tGravityAccMeanY`
+* `tGravityAccMeanZ`
+* `tGravityAccStdX`
+* `tGravityAccStdY`
+* `tGravityAccStdZ`
+* `tBodyAccJerkMeanX`
+* `tBodyAccJerkMeanY`
+* `tBodyAccJerkMeanZ`
+* `tBodyAccJerkStdX`
+* `tBodyAccJerkStdY`
+* `tBodyAccJerkStdZ`
+* `tBodyGyroMeanX`
+* `tBodyGyroMeanY`
+* `tBodyGyroMeanZ`
+* `tBodyGyroStdX`
+* `tBodyGyroStdY`
+* `tBodyGyroStdZ`
+* `tBodyGyroJerkMeanX`
+* `tBodyGyroJerkMeanY`
+* `tBodyGyroJerkMeanZ`
+* `tBodyGyroJerkStdX`
+* `tBodyGyroJerkStdY`
+* `tBodyGyroJerkStdZ`
+* `tBodyAccMagMean`
+* `tBodyAccMagStd`
+* `tGravityAccMagMean`
+* `tGravityAccMagStd`
+* `tBodyAccJerkMagMean`
+* `tBodyAccJerkMagStd`
+* `tBodyGyroMagMean`
+* `tBodyGyroMagStd`
+* `tBodyGyroJerkMagMean`
+* `tBodyGyroJerkMagnitudeStd`
+* `fBodyAccMeanX`
+* `fBodyAccMeanY`
+* `fBodyAccMeanZ`
+* `fBodyAccStdX`
+* `fBodyAccStdY`
+* `fBodyAccStdZ`
+* `fBodyAccMeanFreqX`
+* `fBodyAccMeanFreqY`
+* `fBodyAccMeanFreqZ`
+* `fBodyAccJerkMeanX`
+* `fBodyAccJerkMeanY`
+* `fBodyAccJerkMeanZ`
+* `fBodyAccJerkStdX`
+* `fBodyAccJerkStdY`
+* `fBodyAccJerkStdZ`
+* `fBodyAccJerkMeanFreqX`
+* `fBodyAccJerkMeanFreqY`
+* `fBodyAccJerkMeanFreqZ`
+* `fBodyGyroMeanX`
+* `fBodyGyroMeanY`
+* `fBodyGyroMeanZ`
+* `fBodyGyroStdX`
+* `fBodyGyroStdY`
+* `fBodyGyroStdZ`
+* `fBodyGyroMeanFreqX`
+* `fBodyGyroMeanFreqY`
+* `fBodyGyroMeanFreqZ`
+* `fBodyAccMagnitudeMean`
+* `fBodyAccMagnitudeStd`
+* `fBodyAccMagnitudeMeanFreq`
+* `fBodyBodyAccJerkMagnitudeMean`
+* `fBodyBodyAccJerkMagnitudeStd`
+* `fBodyBodyAccJerkMagnitudeMeanFreq`
+* `fBodyBodyGyroMagnitudeMean`
+* `fBodyBodyGyroMagnitudeStd`
+* `fBodyBodyGyroMagnitudeMeanFreq`
+* `fBodyBodyGyroJerkMagnitudeMean`
+* `fBodyBodyGyroJerkMagnitudeStd`
+* `fBodyBodyGyroJerkMagnitudeMeanFreq`
---
+++ run_analysis.R
@@ -0,0 +1,69 @@
+## Coursera Getting and Cleaning Data Course Project
+## Jesus Rodriguez
+## 2017-11-17
+
+# Script will implement the following steps on the data downloaded from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip:
+# 1. If "UCI HAR Dataset" folder doesn't exist, I will unzip it
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# 3. Merges the training and the test sets to create one data set.
+# 4. Uses descriptive activity names to name the activities in the data set
+# 5. Appropriately labels the data set with descriptive variable names.
+# 6. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+library(reshape2)
+
+## Download data from the URL if file doesn't exist:
+filename <- "get_data.zip"
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename)
+}
+
+# 2. If file doesn't exist, Unzip it  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+# 3. Extracts only the measurements on the mean and standard deviation
+
+## Load activity labels and features
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+activityLabels[,2] <- as.character(activityLabels[,2])
+features <- read.table("UCI HAR Dataset/features.txt")
+features[,2] <- as.character(features[,2])
+
+# Extract only the data on mean and standard deviation
+featuresDesired <- grep(".*mean.*|.*std.*", features[,2])
+featuresDesired.names <- features[featuresDesired,2]
+featuresDesired.names = gsub('-mean', 'Mean', featuresDesired.names)
+featuresDesired.names = gsub('-std', 'Std', featuresDesired.names)
+featuresDesired.names <- gsub('[-()]', '', featuresDesired.names)
+featuresDesired.names <- gsub('*Mag', 'Magnitude', featuresDesired.names)
+
+# 4. Merge datasets and add correct labels
+
+# Loading only featuresDesired from test data into tables
+xTestSet = read.table("UCI HAR Dataset/test/X_test.txt")[featuresDesired]
+subjectTest = read.table("UCI HAR Dataset/test/subject_test.txt")
+ActivitiesTest = read.table("UCI HAR Dataset/test/Y_test.txt")
+xTestSet <- cbind(subjectTest, ActivitiesTest, xTestSet)
+  
+## Loading only featuresDesired from train data into tables
+xTrainSet = read.table("UCI HAR Dataset/train/X_train.txt")[featuresDesired]
+subjectTrain = read.table("UCI HAR Dataset/train/subject_train.txt")
+ActivitiesTrain = read.table("UCI HAR Dataset/train/Y_train.txt")
+xTrainSet <- cbind(subjectTrain, ActivitiesTrain, xTrainSet)
+
+## 5. Merge datasets and add correct labels
+allData <- rbind(xTrainSet, xTestSet)
+colnames(allData) <- c("subject", "activity", featuresWanted.names)
+
+# Turn activities and subjects into factors
+allData$activity <- factor(allData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+allData$subject <- as.factor(allData$subject)
+
+allData.melted <- melt(allData, id = c("subject", "activity"))
+allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
+
+# 6. Export the tidyData set 
+write.table(allData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
